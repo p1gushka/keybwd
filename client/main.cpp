7b578c4c9ee250ca9aa14f50662e1890face43db
p1gushka <sani86188@gmail.com>
2025-06-08 13:10:35 +0300

    annihilated all project to update

--- client/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "view/MainWindow.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    view::MainWindow window;
-    window.show();
-    return app.exec();
-}
